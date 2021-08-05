a30a076ea94273b353cff404d826c17c9fa898c1
jose.neto <netosantosbatista@outlook.com>
2021-08-05 18:31:53 -0300

    removing npgsql and mongodb references

--- ModelAPI.Domain/IUnitOfWork.cs
+++ ModelAPI.Domain/IUnitOfWork.cs
@@ -1,13 +1,11 @@
 using System;
 using System.Data;
 using ModelAPI.Domain.Interfaces.Repository;
-using MongoDB.Driver;
 
 namespace ModelAPI.Domain.Interfaces
 {
     public interface IUnitOfWork : IDisposable
     {
-        IDbConnection Connection { get; }
         IHelloWorldRepository ModelRepository { get; }
         IAuthRepository AuthRepository { get; }
     }
